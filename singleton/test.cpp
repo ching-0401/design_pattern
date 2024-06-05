ab498222f39c62ab5274812db227167c86b0255a
NoWineNoLife <tsing0401@outlook.com>
2024-06-05 21:48:13 +0800

    feat(test singleton && rm unrelative files):

--- singleton/test.cpp
+++ singleton/test.cpp
@@ -2,11 +2,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include <doctest/doctest.h>
+
 #include "singleton.h"
-
-
 
 TEST_CASE("test")
 {
-	CHECK(1 == 1);
+	auto& instance = design_pattern::Singleton::getInstance();
+	CHECK( (&instance) != nullptr);
 }
