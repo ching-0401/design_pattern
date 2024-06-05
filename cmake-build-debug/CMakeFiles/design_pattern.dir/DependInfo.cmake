21e2ae7eadb4248940125ba2d2a2e2aa431f7679
NoWineNoLife <tsing0401@outlook.com>
2024-06-05 21:56:58 +0800

    fix(rm unrelative files):

--- cmake-build-debug/CMakeFiles/design_pattern.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/design_pattern.dir/DependInfo.cmake
@@ -8,13 +8,13 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/yuki/CPlusPlus/design_pattern/main.cpp" "CMakeFiles/design_pattern.dir/main.cpp.o" "gcc" "CMakeFiles/design_pattern.dir/main.cpp.o.d"
-  "/home/yuki/CPlusPlus/design_pattern/src/singleton.cpp" "CMakeFiles/design_pattern.dir/src/singleton.cpp.o" "gcc" "CMakeFiles/design_pattern.dir/src/singleton.cpp.o.d"
-  "/home/yuki/CPlusPlus/design_pattern/src/test.cpp" "CMakeFiles/design_pattern.dir/src/test.cpp.o" "gcc" "CMakeFiles/design_pattern.dir/src/test.cpp.o.d"
+  "/home/yuki/cpp_projects/design_pattern/main.cpp" "CMakeFiles/design_pattern.dir/main.cpp.o" "gcc" "CMakeFiles/design_pattern.dir/main.cpp.o.d"
+  "/home/yuki/cpp_projects/design_pattern/src/singleton.cpp" "CMakeFiles/design_pattern.dir/src/singleton.cpp.o" "gcc" "CMakeFiles/design_pattern.dir/src/singleton.cpp.o.d"
+  "/home/yuki/cpp_projects/design_pattern/src/test.cpp" "CMakeFiles/design_pattern.dir/src/test.cpp.o" "gcc" "CMakeFiles/design_pattern.dir/src/test.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/yuki/CPlusPlus/design_pattern
+# Install script for directory: /home/yuki/cpp_projects/design_pattern
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/yuki/CPlusPlus/design_pattern/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/yuki/cpp_projects/design_pattern/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
